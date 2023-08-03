ebenbruyns/dosbox-staging

Remove the wording "sub-350 modes" used in conjunction with VGA double-scanning
The situation is a bit more involved; e.g. the 256-colour and 16/15-bit
512x384 VESA modes on S3 cards are double-scanned, while the 16-colour
and 24/32-bit variants of the same resolution are *not*.

Programs can also set up custom modes that have less than 350 lines, but
are single-scanned, e.g. the 376x308 @ 86Hz mode of the RealSpec ZX
Spectrum emulator, or the 384x282 @ 95Hz mode of the CCS64 emulator.
Both are single-scanned to emulate scanlines.

This is in preparation for the more accurate double-scan detection
approach that relies on the actual contents of the VGA registers that
control double-scanning.

These very special edge cases might seem unimportant, but the more
accurate approach is actually not more difficult to implement, and it
will likely result in more reliable emulation of weird VGA modes across
the board. Additionaly, this will enable us to remove some existing
hacks/workarounds.

src/gui/sdlmain.cpp
@@ -1709,19 +1709,19 @@ static void check_kmsdrm_setting()
 // double-scanning) but provides finer integer multiplication steps (for sub-4k
 // screens) and also reduces load on slow systems like the Raspberry Pi.
 //
-static void update_vga_sub_350_line_handling([[maybe_unused]] const SCREEN_TYPES screen_type,
-                                             const SCALING_MODE scaling_mode)
+static void update_vga_double_scan_handling([[maybe_unused]] const SCREEN_TYPES screen_type,
+                                            const SCALING_MODE scaling_mode)
 {
-	auto line_handling = VgaSub350LineHandling::SingleScan;
+	auto line_handling = VgaDoubleScanHandling::SingleScan;
 #if C_OPENGL
 	if (screen_type == SCREEN_OPENGL && get_glshader_value() != "none") {
-		line_handling = VgaSub350LineHandling::DoubleScan;
+		line_handling = VgaDoubleScanHandling::DoubleScan;
 	}
 #endif
 	if (scaling_mode == SCALING_MODE::NONE) {
-		line_handling = VgaSub350LineHandling::DoubleScan;
+		line_handling = VgaDoubleScanHandling::DoubleScan;
 	}
-	VGA_SetVgaSub350LineHandling(line_handling);
+	VGA_SetVgaDoubleScanHandling(line_handling);
 }
 
 bool operator!=(const SDL_Point lhs, const SDL_Point rhs)
@@ -2273,7 +2273,7 @@ dosurface:
 	// Ensure mouse emulation knows the current parameters
 	NewMouseScreenParams();
 	update_vsync_state();
-	update_vga_sub_350_line_handling(sdl.desktop.type, sdl.scaling_mode);
+	update_vga_double_scan_handling(sdl.desktop.type, sdl.scaling_mode);
 
 	if (sdl.draw.has_changed)
 		log_display_properties(sdl.draw.width,

dosbox-automation/dosbox-automation

src/gui/sdlmain.cpp
@@ -1718,19 +1718,19 @@ static void check_kmsdrm_setting()
 // double-scanning) but provides finer integer multiplication steps (for sub-4k
 // screens) and also reduces load on slow systems like the Raspberry Pi.
 //
-static void update_vga_sub_350_line_handling([[maybe_unused]] const SCREEN_TYPES screen_type,
-                                             const InterpolationMode interpolation_mode)
+static void update_vga_double_scan_handling([[maybe_unused]] const SCREEN_TYPES screen_type,
+                                            const InterpolationMode interpolation_mode)
 {
-	auto line_handling = VgaSub350LineHandling::SingleScan;
+	auto line_handling = VgaDoubleScanHandling::SingleScan;
 #if C_OPENGL
 	if (screen_type == SCREEN_OPENGL && get_glshader_value() != "none") {
-		line_handling = VgaSub350LineHandling::DoubleScan;
+		line_handling = VgaDoubleScanHandling::DoubleScan;
 	}
 #endif
 	if (interpolation_mode == InterpolationMode::Bilinear) {
-		line_handling = VgaSub350LineHandling::DoubleScan;
+		line_handling = VgaDoubleScanHandling::DoubleScan;
 	}
-	VGA_SetVgaSub350LineHandling(line_handling);
+	VGA_SetVgaDoubleScanHandling(line_handling);
 }
 
 bool operator!=(const SDL_Point lhs, const SDL_Point rhs)
@@ -2283,7 +2283,7 @@ dosurface:
 	// Ensure mouse emulation knows the current parameters
 	NewMouseScreenParams();
 	update_vsync_state();
-	update_vga_sub_350_line_handling(sdl.desktop.type, sdl.interpolation_mode);
+	update_vga_double_scan_handling(sdl.desktop.type, sdl.interpolation_mode);
 
 	if (sdl.draw.has_changed)
 		log_display_properties(sdl.draw.width,