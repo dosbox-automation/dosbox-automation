dosbox-automation/dosbox-automation

commit 5f6c6cc5f93c2cc527baa7c7dac39d57c5b69c18

Allow inactive sections that also don't show in help-texts

src/misc/programs.cpp
@@ -480,17 +480,20 @@ void CONFIG::Run(void)
 					// list the sections
 					WriteOut(MSG_Get("PROGRAM_CONFIG_HLP_SECTLIST"));
 					for (const Section* sec : *control) {
-						WriteOut("  - %s\n", sec->GetName());
+						if (sec->IsActive()) {
+							WriteOut("  - %s\n",
+							         sec->GetName());
+						}
 					}
 					return;
 				}
 				// if it's a section, leave it as one-param
 				Section* sec = control->GetSection(pvars[0]);
-				if (!sec) {
+				if (!sec || !sec->IsActive()) {
 					// could be a property
 					sec = control->GetSectionFromProperty(
 					        pvars[0].c_str());
-					if (!sec) {
+					if (!sec || !sec->IsActive()) {
 						WriteOut(MSG_Get("PROGRAM_CONFIG_PROPERTY_ERROR"),
 						         pvars[0].c_str());
 						return;
@@ -503,12 +506,15 @@ void CONFIG::Run(void)
 			case 2: {
 				// sanity check
 				Section* sec  = control->GetSection(pvars[0]);
+				if (!sec->IsActive()) {
+					sec = nullptr;
+				}
 				Section* sec2 = control->GetSectionFromProperty(
 				        pvars[1].c_str());
-				if (!sec) {
+				if (!sec || !sec->IsActive()) {
 					WriteOut(MSG_Get("PROGRAM_CONFIG_PROPERTY_ERROR"),
 					         pvars[0].c_str());
-				} else if (!sec2 || sec != sec2) {
+				} else if (!sec2 || !sec2->IsActive() || sec != sec2) {
 					WriteOut(MSG_Get("PROGRAM_CONFIG_PROPERTY_ERROR"),
 					         pvars[1].c_str());
 				}
@@ -519,7 +525,7 @@ void CONFIG::Run(void)
 			// if we have one value in pvars, it's a section
 			// two values are section + property
 			Section* sec = control->GetSection(pvars[0]);
-			if (sec == nullptr) {
+			if (sec == nullptr || !sec->IsActive()) {
 				WriteOut(MSG_Get("PROGRAM_CONFIG_PROPERTY_ERROR"),
 				         pvars[0].c_str());
 				return;

include/control.h
@@ -112,6 +112,7 @@ public:
 
 	Section_line* AddSection_line(const char* section_name, SectionFunction func);
 
+	Section_prop* AddInactiveSectionProp(const char* section_name);
 	Section_prop* AddSection_prop(const char* section_name, SectionFunction func,
 	                              bool changeable_at_runtime = false);
 

include/setup.h
@@ -326,10 +326,11 @@ private:
 	std::deque<Function_wrapper> init_functions   = {};
 	std::deque<Function_wrapper> destroyfunctions = {};
 	std::string sectionname                       = {};
+	bool active                                   = true;
 
 public:
 	Section() = default;
-	Section(const std::string& name) : sectionname(name) {}
+	Section(const std::string& name, const bool active = true) : sectionname(name), active(active) {}
 
 	// Construct and assign by std::move
 	Section(Section&& other)            = default;
@@ -345,6 +346,11 @@ public:
 
 	void ExecuteInit(bool initall = true);
 	void ExecuteDestroy(bool destroyall = true);
+
+	bool IsActive() const
+	{
+		return active;
+	}
 
 	const char* GetName() const
 	{
@@ -368,7 +374,7 @@ private:
 	typedef std::deque<Property*>::const_iterator const_it;
 
 public:
-	Section_prop(const std::string& name) : Section(name) {}
+	Section_prop(const std::string& name, bool active = true) : Section(name, active) {}
 
 	~Section_prop() override;
 

src/dosbox.cpp
@@ -1241,14 +1241,22 @@ void DOSBOX_Init()
 	secprop->AddInitFunction(&DRIVES_Init);
 	secprop->AddInitFunction(&CDROM_Image_Init);
 
+#if C_IPX
 	secprop = control->AddSection_prop("ipx", &IPX_Init, changeable_at_runtime);
+#else
+	secprop = control->AddInactiveSectionProp("ipx");
+#endif
 	pbool = secprop->Add_bool("ipx", when_idle, false);
 	pbool->SetOptionHelp("Enable IPX over UDP/IP emulation (disabled by default).");
 #if C_IPX
 	pbool->SetEnabledOptions({"ipx"});
 #endif
 
+#if C_SLIRP
 	secprop = control->AddSection_prop("ethernet", &NE2K_Init, changeable_at_runtime);
+#else
+	secprop = control->AddInactiveSectionProp("ethernet");
+#endif
 
 	pbool = secprop->Add_bool("ne2000", when_idle, true);
 	pbool->SetOptionHelp(

src/misc/setup.cpp
@@ -1223,6 +1223,18 @@ bool Config::WriteConfig(const std_fs::path& path) const
 	return true;
 }
 
+Section_prop* Config::AddInactiveSectionProp(const char* section_name)
+{
+	assertm(std::regex_match(section_name, std::regex{"[a-zA-Z0-9]+"}),
+	        "Only letters and digits are allowed in section name");
+
+	constexpr bool inactive = false;
+	auto s = std::make_unique<Section_prop>(section_name, inactive);
+	auto* section = s.get();
+	sectionlist.push_back(s.release());
+	return section;
+}
+
 Section_prop* Config::AddSection_prop(const char* section_name, SectionFunction func,
                                       bool changeable_at_runtime)
 {

commit 2e4e07fc44f22a0d50e39e1872636f3aeab5c3ca

Fix `CONFIG -h SEC PROP` crash when PROP is non-existent
Fixes regression introduced in 5f6c6cc5f93c2

src/misc/programs.cpp
@@ -501,9 +501,8 @@ void CONFIG::Run(void)
 				break;
 			}
 			case 2: {
-				// sanity check
-				Section* sec  = control->GetSection(pvars[0]);
-				if (!sec->IsActive()) {
+				Section* sec = control->GetSection(pvars[0]);
+				if (sec && !sec->IsActive()) {
 					sec = nullptr;
 				}
 				Section* sec2 = control->GetSectionFromProperty(