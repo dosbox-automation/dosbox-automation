dosbox-automation/dosbox-automation

commit e200ac179496dd6a83c514b35f2848026d78c308

Move the Mixer's management of audio device queues behind API calls
This decreases coupling by hiding internal details, and matches the
existing module design pattern in DOSBox.

src/hardware/mixer.cpp
@@ -38,22 +38,16 @@
 #include "checks.h"
 #include "control.h"
 #include "cross.h"
-#include "gus.h"
 #include "hardware.h"
-#include "lpt_dac.h"
 #include "mapper.h"
 #include "math_utils.h"
 #include "mem.h"
 #include "midi.h"
-#include "pcspeaker.h"
 #include "pic.h"
-#include "ps1audio.h"
-#include "reelmagic/player.h"
 #include "ring_buffer.h"
 #include "rwqueue.h"
 #include "setup.h"
 #include "string_utils.h"
-#include "tandy_sound.h"
 #include "timer.h"
 #include "tracy.h"
 #include "video.h"
@@ -295,45 +289,30 @@ bool MIXER_FastForwardModeEnabled()
 // Individual channels also have a mutex which can be safely aquired without stopping these queues
 void MIXER_LockMixerThread()
 {
-	if (pc_speaker) {
-		pc_speaker->output_queue.Stop();
-	}
-	if (tandy_dac) {
-		tandy_dac->output_queue.Stop();
-	}
-	if (ps1_dac) {
-		ps1_dac->output_queue.Stop();
-	}
-	if (lpt_dac) {
-		lpt_dac->output_queue.Stop();
-	}
-	if (gus) {
-		gus->output_queue.Stop();
-	}
-	soundblaster_mixer_queue.Stop();
-	reel_magic_audio.output_queue.Stop();
+	PCSPEAKER_NotifyLockMixer();
+
+	TANDYDAC_NotifyLockMixer();
+
+	PS1DAC_NotifyLockMixer();
+
+	GUS_NotifyLockMixer();
+
+	REELMAGIC_NotifyLockMixer();
+
 	mixer.mutex.lock();
 }
 
 void MIXER_UnlockMixerThread()
 {
-	if (pc_speaker) {
-		pc_speaker->output_queue.Start();
-	}
-	if (tandy_dac) {
-		tandy_dac->output_queue.Start();
-	}
-	if (ps1_dac) {
-		ps1_dac->output_queue.Start();
-	}
-	if (lpt_dac) {
-		lpt_dac->output_queue.Start();
-	}
-	if (gus) {
-		gus->output_queue.Start();
-	}
-	soundblaster_mixer_queue.Start();
-	reel_magic_audio.output_queue.Start();
+	PCSPEAKER_NotifyUnlockMixer();
+
+	TANDYDAC_NotifyUnlockMixer();
+
+	PS1DAC_NotifyUnlockMixer();
+
+	GUS_NotifyUnlockMixer();
+
+	REELMAGIC_NotifyUnlockMixer();
 	mixer.mutex.unlock();
 }
 

include/hardware.h
@@ -67,4 +67,23 @@ void IMFC_AddConfigSection(const ConfigPtr& conf);
 // Innovation SSI-2001 configuration and initialisation
 void INNOVATION_AddConfigSection(const ConfigPtr &conf);
 
+// Common lock notification calls used by the mixer
+void PCSPEAKER_NotifyLockMixer();
+void PCSPEAKER_NotifyUnlockMixer();
+
+void TANDYDAC_NotifyLockMixer();
+void TANDYDAC_NotifyUnlockMixer();
+
+void PS1DAC_NotifyLockMixer();
+void PS1DAC_NotifyUnlockMixer();
+
+void GUS_NotifyLockMixer();
+void GUS_NotifyUnlockMixer();
+
+void SBLASTER_NotifyLockMixer();
+void SBLASTER_NotifyUnlockMixer();
+
+void REELMAGIC_NotifyLockMixer();
+void REELMAGIC_NotifyUnlockMixer();
+
 #endif

src/hardware/gus.cpp
@@ -1483,6 +1483,19 @@ void GUS_MirrorAdLibCommandPortWrite([[maybe_unused]] const io_port_t port,
 	}
 }
 
+void GUS_NotifyLockMixer()
+{
+	if (gus) {
+		gus->output_queue.Stop();
+	}
+}
+void GUS_NotifyUnlockMixer()
+{
+	if (gus) {
+		gus->output_queue.Start();
+	}
+}
+
 static void gus_destroy([[maybe_unused]] Section* sec)
 {
 	// GUS destroy is run when the user wants to deactivate the GUS:

src/hardware/gus.h
@@ -385,5 +385,3 @@ private:
 	bool irq_previously_interrupted = false;
 	bool should_change_irq_dma      = false;
 };
-
-extern std::unique_ptr<Gus> gus;

src/hardware/pcspeaker.cpp
@@ -126,3 +126,16 @@ void PCSPEAKER_SetType(const PpiPortB &port_b)
 	if (pc_speaker)
 		pc_speaker->SetType(port_b);
 }
+
+void PCSPEAKER_NotifyLockMixer()
+{
+	if (pc_speaker) {
+		pc_speaker->output_queue.Stop();
+	}
+}
+void PCSPEAKER_NotifyUnlockMixer()
+{
+	if (pc_speaker) {
+		pc_speaker->output_queue.Start();
+	}
+}

src/hardware/pcspeaker.h
@@ -45,6 +45,4 @@ public:
 	virtual void PicCallback(const int requested_frames)            = 0;
 };
 
-extern std::unique_ptr<PcSpeaker> pc_speaker;
-
 #endif

src/hardware/ps1audio.cpp
@@ -44,6 +44,8 @@
 
 CHECK_NARROWING();
 
+std::unique_ptr<Ps1Dac> ps1_dac = {};
+
 static void setup_filter(MixerChannelPtr& channel, const bool filter_enabled)
 {
 	if (filter_enabled) {
@@ -421,6 +423,8 @@ private:
 	double last_rendered_ms = 0.0;
 };
 
+static std::unique_ptr<Ps1Synth> ps1_synth = {};
+
 Ps1Synth::Ps1Synth(const std::string& filter_choice)
         : device(nullptr, nullptr, Ps1PsgClockHz)
 {
@@ -553,8 +557,18 @@ Ps1Synth::~Ps1Synth()
 	MIXER_UnlockMixerThread();
 }
 
-std::unique_ptr<Ps1Dac> ps1_dac            = {};
-static std::unique_ptr<Ps1Synth> ps1_synth = {};
+void PS1DAC_NotifyLockMixer()
+{
+	if (ps1_dac) {
+		ps1_dac->output_queue.Stop();
+	}
+}
+void PS1DAC_NotifyUnlockMixer()
+{
+	if (ps1_dac) {
+		ps1_dac->output_queue.Start();
+	}
+}
 
 static void PS1AUDIO_ShutDown([[maybe_unused]] Section* sec)
 {

src/hardware/ps1audio.h
@@ -114,6 +114,4 @@ private:
 	bool can_trigger_irq = false;
 };
 
-extern std::unique_ptr<Ps1Dac> ps1_dac;
-
 #endif

commit 4a68ce716fca7a2f3ee28636c97542a9fcf956b6

Fix SoundBlaster Notify Unlock
Typo in MIXER_UnlockMixer() was stopping the SoundBlaster queue instead of starting it
This caused an assertion failure when resuming from pause and possibly other problems that went unnoticed

Regression from e200ac179496dd6a83c514b35f2848026d78c308 (refactor to use device locking functions, removed the original SB Start/Stop)
Next commit 96d3ed5145391184fafc4c93d7a98810c0555b04 had the typo of Lock instead of Unlock

src/hardware/mixer.cpp
@@ -312,7 +312,7 @@ void MIXER_UnlockMixerThread()
 	LPTDAC_NotifyUnlockMixer();
 	GUS_NotifyUnlockMixer();
 	REELMAGIC_NotifyUnlockMixer();
-	SBLASTER_NotifyLockMixer();
+	SBLASTER_NotifyUnlockMixer();
 
 	mixer.mutex.unlock();
 }