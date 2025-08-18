dosbox-staging/dosbox-staging

Fix not allowing to change `[sdl]` section settings after init

src/gui/sdlmain.cpp
@@ -3382,8 +3382,17 @@ static void set_fullscreen_mode()
 	}
 }
 
-static void read_gui_config(Section* sec)
+static void sdl_section_init(Section* sec)
 {
+	assert(sec);
+
+	const Section_prop* conf = dynamic_cast<Section_prop*>(sec);
+	assert(conf);
+
+	if (!conf) {
+		return;
+	}
+
 	sec->AddDestroyFunction(&shutdown_gui);
 	Section_prop* section = static_cast<Section_prop*>(sec);
 
@@ -3513,22 +3522,6 @@ static void read_gui_config(Section* sec)
 
 	// Notify MOUSE subsystem that it can start now
 	MOUSE_NotifyReadyGFX();
-}
-
-static void sdl_section_init(Section* sec)
-{
-	assert(sec);
-	const Section_prop* conf = dynamic_cast<Section_prop*>(sec);
-	assert(conf);
-	if (!conf) {
-		return;
-	}
-
-	static bool first_time = true;
-	if (first_time) {
-		read_gui_config(sec);
-		first_time = false;
-	}
 
 	TITLEBAR_ReadConfig(*conf);
 }

dosbox-automation/dosbox-automation

src/gui/sdlmain.cpp
@@ -3385,8 +3385,17 @@ static void set_fullscreen_mode()
 	}
 }
 
-static void read_gui_config(Section* sec)
+static void sdl_section_init(Section* sec)
 {
+	assert(sec);
+
+	const SectionProp* conf = dynamic_cast<SectionProp*>(sec);
+	assert(conf);
+
+	if (!conf) {
+		return;
+	}
+
 	sec->AddDestroyFunction(&shutdown_gui);
 	SectionProp* section = static_cast<SectionProp*>(sec);
 
@@ -3516,22 +3525,6 @@ static void read_gui_config(Section* sec)
 
 	// Notify MOUSE subsystem that it can start now
 	MOUSE_NotifyReadyGFX();
-}
-
-static void sdl_section_init(Section* sec)
-{
-	assert(sec);
-	const SectionProp* conf = dynamic_cast<SectionProp*>(sec);
-	assert(conf);
-	if (!conf) {
-		return;
-	}
-
-	static bool first_time = true;
-	if (first_time) {
-		read_gui_config(sec);
-		first_time = false;
-	}
 
 	TITLEBAR_ReadConfig(*conf);
 }