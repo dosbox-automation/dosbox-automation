dosbox-staging/dosbox-staging

Implement gettext file format support for translations
This particular commit can be used to convert the legacy translation
files to the gettext format, for example:

config -set language=fr.lng
config -wl ./resources/translations/fr.po

src/misc/messages.h
@@ -7,6 +7,24 @@
 #include <cstdint>
 #include <string>
 
+class MsgLocation {
+public:
+	MsgLocation(const std::string& file_name, const size_t line_number)
+	        : file_name(file_name),
+	          line_number(line_number)
+	{}
+
+	bool operator==(const MsgLocation& other) const;
+
+	std::string GetUnified() const;
+
+private:
+	MsgLocation() = delete;
+
+	const std::string file_name = {};
+	const size_t line_number    = 0;
+};
+
 /*
  * Load translated messages according to the configuration.
  */
@@ -16,7 +34,12 @@ void MSG_LoadMessages();
  * Add English message to the translation system in UTF-8.
  * The message may contais ANSI markup tags (e.g., [white]CONFIG[reset]).
  */
-void MSG_Add(const std::string& name, const std::string& message);
+void MSG_Add(const std::string& message_key, const std::string& message,
+             const MsgLocation& location = {__builtin_FILE(), __builtin_LINE()});
+
+// TODO: Use 'std::source_location::current()' once all the supported compilers
+// implement this C++20 feature; it should be possible to move 'MsgLocation'
+// declaration to 'messages.cpp' then.
 
 /*
  * Get translated message preprocessed for output to the DOS console.

src/misc/messages_stubs.cpp
@@ -8,7 +8,7 @@
 
 void MSG_NotifyNewCodePage() {}
 
-void MSG_Add(const std::string&, const std::string&) {}
+void MSG_Add(const std::string&, const std::string&, const MsgLocation&) {}
 
 std::string MSG_Get(const std::string&)
 {

dosbox-automation/dosbox-automation

src/misc/messages.h
@@ -7,6 +7,24 @@
 #include <cstdint>
 #include <string>
 
+class MessageLocation {
+public:
+	MessageLocation(const std::string& file_name, const size_t line_number)
+	        : file_name(file_name),
+	          line_number(line_number)
+	{}
+
+	bool operator==(const MessageLocation& other) const;
+
+	std::string GetUnified() const;
+
+private:
+	MessageLocation() = delete;
+
+	const std::string file_name = {};
+	const size_t line_number    = 0;
+};
+
 /*
  * Load translated messages according to the configuration.
  */
@@ -16,7 +34,12 @@ void MSG_LoadMessages();
  * Add English message to the translation system in UTF-8.
  * The message may contais ANSI markup tags (e.g., [white]CONFIG[reset]).
  */
-void MSG_Add(const std::string& name, const std::string& message);
+void MSG_Add(const std::string& message_key, const std::string& message,
+             const MessageLocation& location = {__builtin_FILE(), __builtin_LINE()});
+
+// TODO: Use 'std::source_location::current()' once all the supported compilers
+// implement this C++20 feature; it should be possible to move 'MsgLocation'
+// declaration to 'messages.cpp' then.
 
 /*
  * Get translated message preprocessed for output to the DOS console.

src/misc/messages_stubs.cpp
@@ -8,7 +8,7 @@
 
 void MSG_NotifyNewCodePage() {}
 
-void MSG_Add(const std::string&, const std::string&) {}
+void MSG_Add(const std::string&, const std::string&, const MessageLocation&) {}
 
 std::string MSG_Get(const std::string&)
 {