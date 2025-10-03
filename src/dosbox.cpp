dosbox-staging/dosbox-staging

Move `[ethernet]` section init to `ethernet.cpp`

src/dosbox.cpp
@@ -41,6 +41,7 @@
 #include "ints/int10.h"
 #include "midi/midi.h"
 #include "misc/cross.h"
+#include "misc/ethernet.h"
 #include "misc/support.h"
 #include "misc/tracy.h"
 #include "misc/video.h"
@@ -57,9 +58,6 @@ void IO_Init(Section * );
 void CALLBACK_Init(Section*);
 
 void DOS_Init(Section*);
-
-void VIRTUALBOX_Init(Section*);
-void VMWARE_Init(Section*);
 
 void PCSPEAKER_Init(Section*);
 void TANDYSOUND_Init(Section*);
@@ -1091,93 +1089,6 @@ static void add_dos_section()
 	secprop->AddInitHandler(CDROM_Image_Init);
 }
 
-static void add_ethernet_section()
-{
-	using enum Property::Changeable::Value;
-
-	constexpr auto changeable_at_runtime = true;
-	auto secprop = control->AddSection("ethernet", NE2K_Init, changeable_at_runtime);
-
-	auto pbool = secprop->AddBool("ne2000", WhenIdle, false);
-	pbool->SetOptionHelp(
-	        "SLIRP",
-	        "Enable emulation of a Novell NE2000 network card on a software-based network\n"
-	        "with the following properties ('off' by default):\n"
-	        "  - 255.255.255.0   Subnet mask of the 10.0.2.0 virtual LAN.\n"
-	        "  - 10.0.2.2        IP of the gateway and DHCP service.\n"
-	        "  - 10.0.2.3        IP of the virtual DNS server.\n"
-	        "  - 10.0.2.15       First IP provided by DHCP (this is your IP)\n"
-	        "Note: Using this feature requires an NE2000 packet driver, a DHCP client, and a\n"
-	        "      TCP/IP stack set up in DOS. You might need port-forwarding from your host\n"
-	        "      OS into DOSBox, and from your router to your host OS when acting as the\n"
-	        "      server in multiplayer games.");
-
-	pbool->SetEnabledOptions({"SLIRP"});
-
-	auto phex = secprop->AddHex("nicbase", WhenIdle, 0x300);
-	phex->SetValues(
-	        {"200", "220", "240", "260", "280", "2c0", "300", "320", "340", "360"});
-	phex->SetOptionHelp("SLIRP",
-	                    "Base address of the NE2000 card (300 by default).\n"
-	                    "Note: Addresses 220 and 240 might not be available as they're assigned to the\n"
-	                    "      Sound Blaster and Gravis UltraSound by default.");
-
-	phex->SetEnabledOptions({"SLIRP"});
-
-	auto pint = secprop->AddInt("nicirq", WhenIdle, 3);
-	pint->SetValues({"3", "4", "5", "9", "10", "11", "12", "14", "15"});
-	pint->SetOptionHelp("SLIRP",
-	                    "The interrupt used by the NE2000 card (3 by default).\n"
-	                    "Note: IRQs 3 and 5 might not be available as they're assigned to\n"
-	                    "      'serial2' and the Gravis UltraSound by default.");
-
-	pint->SetEnabledOptions({"SLIRP"});
-
-	auto pstring = secprop->AddString("macaddr", WhenIdle, "AC:DE:48:88:99:AA");
-	pstring->SetOptionHelp("SLIRP",
-	                       "The MAC address of the NE2000 card ('AC:DE:48:88:99:AA' by default).");
-
-	pstring->SetEnabledOptions({"SLIRP"});
-
-	pstring = secprop->AddString("tcp_port_forwards", WhenIdle, "");
-	pstring->SetOptionHelp(
-	        "SLIRP",
-	        "Forward one or more TCP ports from the host into the DOS guest\n"
-	        "(unset by default).\n"
-	        "The format is:\n"
-	        "  port1  port2  port3 ... (e.g., 21 80 443)\n"
-	        "  This will forward FTP, HTTP, and HTTPS into the DOS guest.\n"
-	        "If the ports are privileged on the host, a mapping can be used\n"
-	        "  host:guest  ..., (e.g., 8021:21 8080:80)\n"
-	        "  This will forward ports 8021 and 8080 to FTP and HTTP in the guest.\n"
-	        "A range of adjacent ports can be abbreviated with a dash:\n"
-	        "  start-end ... (e.g., 27910-27960)\n"
-	        "  This will forward ports 27910 to 27960 into the DOS guest.\n"
-	        "Mappings and ranges can be combined, too:\n"
-	        "  hstart-hend:gstart-gend ..., (e.g, 8040-8080:20-60)\n"
-	        "  This forwards ports 8040 to 8080 into 20 to 60 in the guest.\n"
-	        "Notes:\n"
-	        "  - If mapped ranges differ, the shorter range is extended to fit.\n"
-	        "  - If conflicting host ports are given, only the first one is setup.\n"
-	        "  - If conflicting guest ports are given, the latter rule takes precedent.");
-
-	pstring->SetEnabledOptions({"SLIRP"});
-
-	pstring = secprop->AddString("udp_port_forwards", WhenIdle, "");
-	pstring->SetOptionHelp(
-	        "SLIRP",
-	        "Forward one or more UDP ports from the host into the DOS guest\n"
-	        "(unset by default). The format is the same as for TCP port forwards.");
-
-	pstring->SetEnabledOptions({"SLIRP"});
-
-	//	secprop->AddInitHandler(&CREDITS_Init);
-
-	// VMM interfaces
-	secprop->AddInitHandler(VIRTUALBOX_Init);
-	secprop->AddInitHandler(VMWARE_Init);
-}
-
 void DOSBOX_InitAllModuleConfigsAndMessages()
 {
 	// The [sdl] section gets initialised first in `sdlmain.cpp`, then
@@ -1249,7 +1160,7 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 	IPX_AddConfigSection(control);
 #endif
 
-	add_ethernet_section();
+	ETHERNET_AddConfigSection(control);
 
 	control->AddAutoexecSection(AUTOEXEC_Init);
 

dosbox-automation/dosbox-automation

src/dosbox.cpp
@@ -47,6 +47,7 @@
 #include "misc/support.h"
 #include "misc/tracy.h"
 #include "misc/video.h"
+#include "network/ethernet.h"
 #include "shell/shell.h"
 #include "utils/math_utils.h"
 
@@ -60,9 +61,6 @@ void IO_Init(Section * );
 void CALLBACK_Init(Section*);
 
 void DOS_Init(Section*);
-
-void VIRTUALBOX_Init(Section*);
-void VMWARE_Init(Section*);
 
 void PCSPEAKER_Init(Section*);
 void TANDYSOUND_Init(Section*);
@@ -1094,93 +1092,6 @@ static void add_dos_section()
 	secprop->AddInitHandler(CDROM_Image_Init);
 }
 
-static void add_ethernet_section()
-{
-	using enum Property::Changeable::Value;
-
-	constexpr auto changeable_at_runtime = true;
-	auto secprop = control->AddSection("ethernet", NE2K_Init, changeable_at_runtime);
-
-	auto pbool = secprop->AddBool("ne2000", WhenIdle, false);
-	pbool->SetOptionHelp(
-	        "SLIRP",
-	        "Enable emulation of a Novell NE2000 network card on a software-based network\n"
-	        "with the following properties ('off' by default):\n"
-	        "  - 255.255.255.0   Subnet mask of the 10.0.2.0 virtual LAN.\n"
-	        "  - 10.0.2.2        IP of the gateway and DHCP service.\n"
-	        "  - 10.0.2.3        IP of the virtual DNS server.\n"
-	        "  - 10.0.2.15       First IP provided by DHCP (this is your IP)\n"
-	        "Note: Using this feature requires an NE2000 packet driver, a DHCP client, and a\n"
-	        "      TCP/IP stack set up in DOS. You might need port-forwarding from your host\n"
-	        "      OS into DOSBox, and from your router to your host OS when acting as the\n"
-	        "      server in multiplayer games.");
-
-	pbool->SetEnabledOptions({"SLIRP"});
-
-	auto phex = secprop->AddHex("nicbase", WhenIdle, 0x300);
-	phex->SetValues(
-	        {"200", "220", "240", "260", "280", "2c0", "300", "320", "340", "360"});
-	phex->SetOptionHelp("SLIRP",
-	                    "Base address of the NE2000 card (300 by default).\n"
-	                    "Note: Addresses 220 and 240 might not be available as they're assigned to the\n"
-	                    "      Sound Blaster and Gravis UltraSound by default.");
-
-	phex->SetEnabledOptions({"SLIRP"});
-
-	auto pint = secprop->AddInt("nicirq", WhenIdle, 3);
-	pint->SetValues({"3", "4", "5", "9", "10", "11", "12", "14", "15"});
-	pint->SetOptionHelp("SLIRP",
-	                    "The interrupt used by the NE2000 card (3 by default).\n"
-	                    "Note: IRQs 3 and 5 might not be available as they're assigned to\n"
-	                    "      'serial2' and the Gravis UltraSound by default.");
-
-	pint->SetEnabledOptions({"SLIRP"});
-
-	auto pstring = secprop->AddString("macaddr", WhenIdle, "AC:DE:48:88:99:AA");
-	pstring->SetOptionHelp("SLIRP",
-	                       "The MAC address of the NE2000 card ('AC:DE:48:88:99:AA' by default).");
-
-	pstring->SetEnabledOptions({"SLIRP"});
-
-	pstring = secprop->AddString("tcp_port_forwards", WhenIdle, "");
-	pstring->SetOptionHelp(
-	        "SLIRP",
-	        "Forward one or more TCP ports from the host into the DOS guest\n"
-	        "(unset by default).\n"
-	        "The format is:\n"
-	        "  port1  port2  port3 ... (e.g., 21 80 443)\n"
-	        "  This will forward FTP, HTTP, and HTTPS into the DOS guest.\n"
-	        "If the ports are privileged on the host, a mapping can be used\n"
-	        "  host:guest  ..., (e.g., 8021:21 8080:80)\n"
-	        "  This will forward ports 8021 and 8080 to FTP and HTTP in the guest.\n"
-	        "A range of adjacent ports can be abbreviated with a dash:\n"
-	        "  start-end ... (e.g., 27910-27960)\n"
-	        "  This will forward ports 27910 to 27960 into the DOS guest.\n"
-	        "Mappings and ranges can be combined, too:\n"
-	        "  hstart-hend:gstart-gend ..., (e.g, 8040-8080:20-60)\n"
-	        "  This forwards ports 8040 to 8080 into 20 to 60 in the guest.\n"
-	        "Notes:\n"
-	        "  - If mapped ranges differ, the shorter range is extended to fit.\n"
-	        "  - If conflicting host ports are given, only the first one is setup.\n"
-	        "  - If conflicting guest ports are given, the latter rule takes precedent.");
-
-	pstring->SetEnabledOptions({"SLIRP"});
-
-	pstring = secprop->AddString("udp_port_forwards", WhenIdle, "");
-	pstring->SetOptionHelp(
-	        "SLIRP",
-	        "Forward one or more UDP ports from the host into the DOS guest\n"
-	        "(unset by default). The format is the same as for TCP port forwards.");
-
-	pstring->SetEnabledOptions({"SLIRP"});
-
-	//	secprop->AddInitHandler(&CREDITS_Init);
-
-	// VMM interfaces
-	secprop->AddInitHandler(VIRTUALBOX_Init);
-	secprop->AddInitHandler(VMWARE_Init);
-}
-
 void DOSBOX_InitAllModuleConfigsAndMessages()
 {
 	// The [sdl] section gets initialised first in `sdlmain.cpp`, then
@@ -1252,7 +1163,7 @@ void DOSBOX_InitAllModuleConfigsAndMessages()
 	IPX_AddConfigSection(control);
 #endif
 
-	add_ethernet_section();
+	ETHERNET_AddConfigSection(control);
 
 	control->AddAutoexecSection(AUTOEXEC_Init);
 