dosbox-staging/dosbox-staging

Clean up I/O port init

src/hardware/iohandler.cpp
@@ -3,8 +3,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <cassert>
-#include <limits>
 #include <cstring>
+#include <limits>
+#include <memory>
 #include <unordered_map>
 
 #include "config/setup.h"
@@ -12,7 +13,6 @@
 #include "cpu/cpu.h"
 #include "cpu/lazyflags.h"
 #include "hardware/port.h"
-
 
 //#define ENABLE_PORTLOG
 
@@ -385,13 +385,15 @@ public:
 	}
 };
 
-static IO* test;
+static std::unique_ptr<IO> io_module = {};
 
-void IO_Destroy(Section*) {
-	delete test;
+static void io_destroy(Section*)
+{
+	io_module = {};
 }
 
-void IO_Init(Section * sect) {
-	test = new IO(sect);
-	sect->AddDestroyHandler(IO_Destroy);
+void IO_Init(Section* sec)
+{
+	io_module = std::make_unique<IO>(sec);
+	sect->AddDestroyHandler(io_destroy);
 }

dosbox-automation/dosbox-automation

src/hardware/iohandler.cpp
@@ -3,8 +3,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include <cassert>
-#include <limits>
 #include <cstring>
+#include <limits>
+#include <memory>
 #include <unordered_map>
 
 #include "config/setup.h"
@@ -12,7 +13,6 @@
 #include "cpu/cpu.h"
 #include "cpu/lazyflags.h"
 #include "hardware/port.h"
-
 
 //#define ENABLE_PORTLOG
 
@@ -385,13 +385,15 @@ public:
 	}
 };
 
-static IO* test;
+static std::unique_ptr<IO> io_module = {};
 
-void IO_Destroy(Section*) {
-	delete test;
+static void io_destroy(Section*)
+{
+	io_module = {};
 }
 
-void IO_Init(Section * sect) {
-	test = new IO(sect);
-	sect->AddDestroyHandler(IO_Destroy);
+void IO_Init(Section* sec)
+{
+	io_module = std::make_unique<IO>(sec);
+	sec->AddDestroyHandler(io_destroy);
 }