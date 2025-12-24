dosbox-automation/dosbox-automation

commit 5c9bd249140303d4fe8b265bd0ce49d5ed1d8789

Initialise DOSBox modules before the GUI
This fixes the crash at startup when setting `shader = crt-auto-machine`
in the config, and it's the more correct init order anyhway.

src/main.cpp
@@ -671,10 +671,10 @@ int main(int argc, char* argv[])
 
 		maybe_create_resource_directories();
 
+		DOSBOX_InitModules();
+
 		// Initialise the GUI
 		GFX_Init();
-
-		DOSBOX_InitModules();
 
 		// All subsystems' hotkeys need to be registered at this point
 		// to ensure their hotkeys appear in the graphical mapper.
@@ -688,8 +688,8 @@ int main(int argc, char* argv[])
 		SHELL_InitAndRun();
 
 		// Shutdown and release
-		GFX_Destroy();
 		DOSBOX_DestroyModules();
+		GFX_Destroy();
 
 	} catch (char* error) {
 		// TODO Maybe show popup dialog with the error in addition to

commit c0dff4d8e4e1fe0fb7ab1722b1ff14be8c05ae93

Fix init order
Initialise all SDL modules first, then the emulation, then start the
GUI.

This fixes the no sound regression introduced by 5c9bd2491.

src/gui/common.h
@@ -15,7 +15,8 @@
 
 void GFX_AddConfigSection();
 
-void GFX_Init();
+void GFX_InitSdl();
+void GFX_InitAndStartGui();
 void GFX_Destroy();
 
 void GFX_RequestExit(const bool pressed);

src/gui/sdl_gui.cpp
@@ -1709,12 +1709,12 @@ static void handle_macos_dosbox_package_drop(const std::string& dropped_file_pat
 }
 #endif
 
-void GFX_Init()
+void GFX_InitSdl()
 {
 	set_sdl_hints();
 
 	// Initialise SDL (timer is needed for title bar animations)
-	if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
+	if (SDL_InitSubSystem(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0) {
 		E_Exit("SDL: Can't init SDL %s", SDL_GetError());
 	}
 
@@ -1756,7 +1756,10 @@ void GFX_Init()
 		}
 	}
 #endif
+}
 
+void GFX_InitAndStartGui()
+{
 	// Start GUI init
 	configure_pause_and_mute_when_inactive();
 

src/main.cpp
@@ -671,10 +671,12 @@ int main(int argc, char* argv[])
 
 		maybe_create_resource_directories();
 
+		GFX_InitSdl();
+
 		DOSBOX_InitModules();
 
-		// Initialise the GUI
-		GFX_Init();
+		// Initialise and start the GUI
+		GFX_InitAndStartGui();
 
 		// All subsystems' hotkeys need to be registered at this point
 		// to ensure their hotkeys appear in the graphical mapper.