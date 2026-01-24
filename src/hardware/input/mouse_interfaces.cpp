dosbox-staging/dosbox-staging

Make 'mouse_interfaces' private of 'mouse_interfaces.cpp'
This also fixes config options like 'serial1 = mouse', which got
broken during the startup order cleanup

src/hardware/input/mouse_interfaces.cpp
@@ -14,20 +14,20 @@
 
 CHECK_NARROWING();
 
-std::vector<std::unique_ptr<MouseInterface>> mouse_interfaces = {};
+constexpr auto num_interfaces = AllMouseInterfaceIds.size();
+static std::array<MouseInterface*, num_interfaces> mouse_interfaces = { nullptr };
 
 // ***************************************************************************
 // Mouse interface information facade
 // ***************************************************************************
 
 MouseInterfaceInfoEntry::MouseInterfaceInfoEntry(const MouseInterfaceId interface_id)
-        : interface_idx(static_cast<uint8_t>(interface_id))
+        : interface_id(interface_id)
 {}
 
 const MouseInterface& MouseInterfaceInfoEntry::Interface() const
 {
-	assert(mouse_interfaces[interface_idx]);
-	return *mouse_interfaces[interface_idx].get();
+	return MouseInterface::GetInstance(interface_id);
 }
 
 const MousePhysical& MouseInterfaceInfoEntry::MappedPhysical() const
@@ -216,84 +216,42 @@ private:
 
 void MouseInterface::InitAllInstances()
 {
-	if (!mouse_interfaces.empty()) {
-		return; // already initialized
+	for (auto interface_id : AllMouseInterfaceIds) {
+		auto& interface = MouseInterface::GetInstance(interface_id);
+		interface.Init();
+		interface.UpdateConfig();
 	}
+}
 
-	const auto first = static_cast<uint8_t>(MouseInterfaceId::First);
-	const auto last  = static_cast<uint8_t>(MouseInterfaceId::Last);
+MouseInterface& MouseInterface::GetInstance(const MouseInterfaceId interface_id)
+{
+	const auto index = enum_val(interface_id);
+	if (!mouse_interfaces[index]) {
 
-	for (uint8_t i = first; i <= last; i++) {
-		switch (static_cast<MouseInterfaceId>(i)) {
+		switch (interface_id) {
 		case MouseInterfaceId::DOS:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceDos>());
+			mouse_interfaces[index] = new InterfaceDos();
 			break;
 		case MouseInterfaceId::PS2:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfacePS2>());
+			mouse_interfaces[index] = new InterfacePS2();
 			break;
 		case MouseInterfaceId::COM1:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceCOM>(0));
+			mouse_interfaces[index] = new InterfaceCOM(0);
 			break;
 		case MouseInterfaceId::COM2:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceCOM>(1));
+			mouse_interfaces[index] = new InterfaceCOM(1);
 			break;
 		case MouseInterfaceId::COM3:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceCOM>(2));
+			mouse_interfaces[index] = new InterfaceCOM(2);
 			break;
 		case MouseInterfaceId::COM4:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceCOM>(3));
+			mouse_interfaces[index] = new InterfaceCOM(3);
 			break;
 		default: assert(false); break;
 		}
 	}
 
-	for (auto& interface : mouse_interfaces) {
-		assert(interface);
-		interface->Init();
-		interface->UpdateConfig();
-	}
-}
-
-MouseInterface* MouseInterface::Get(const MouseInterfaceId interface_id)
-{
-	const auto idx = static_cast<size_t>(interface_id);
-	if (idx < mouse_interfaces.size()) {
-		assert(mouse_interfaces[idx]);
-		return mouse_interfaces[idx].get();
-	}
-
-	assert(interface_id == MouseInterfaceId::None);
-	return nullptr;
-}
-
-MouseInterface* MouseInterface::GetDOS()
-{
-	const auto idx = static_cast<uint8_t>(MouseInterfaceId::DOS);
-	return MouseInterface::Get(static_cast<MouseInterfaceId>(idx));
-}
-
-MouseInterface* MouseInterface::GetPS2()
-{
-	const auto idx = static_cast<uint8_t>(MouseInterfaceId::PS2);
-	return MouseInterface::Get(static_cast<MouseInterfaceId>(idx));
-}
-
-MouseInterface* MouseInterface::GetSerial(const uint8_t port_id)
-{
-	if (port_id < SERIAL_MAX_PORTS) {
-		const auto idx = static_cast<uint8_t>(MouseInterfaceId::COM1) + port_id;
-		return MouseInterface::Get(static_cast<MouseInterfaceId>(idx));
-	}
-
-	LOG_ERR("MOUSE: Ports above COM4 not supported");
-	assert(false);
-	return nullptr;
+	return *mouse_interfaces[index];
 }
 
 MouseInterface::MouseInterface(const MouseInterfaceId interface_id,
@@ -382,6 +340,12 @@ void MouseInterface::NotifyInterfaceRate(const uint16_t new_rate_hz)
 	interface_rate_hz = new_rate_hz;
 	UpdateRate();
 }
+
+void MouseInterface::NotifyMoved(const float, const float, const float, const float) {}
+
+void MouseInterface::NotifyButton(const MouseButtonId, const bool) {}
+
+void MouseInterface::NotifyWheel(const float) {}
 
 void MouseInterface::NotifyBooting() {}
 

dosbox-automation/dosbox-automation

src/hardware/input/mouse_interfaces.cpp
@@ -14,20 +14,20 @@
 
 CHECK_NARROWING();
 
-std::vector<std::unique_ptr<MouseInterface>> mouse_interfaces = {};
+constexpr auto NumInterfaces = AllMouseInterfaceIds.size();
+static std::array<MouseInterface*, NumInterfaces> mouse_interfaces = { nullptr };
 
 // ***************************************************************************
 // Mouse interface information facade
 // ***************************************************************************
 
 MouseInterfaceInfoEntry::MouseInterfaceInfoEntry(const MouseInterfaceId interface_id)
-        : interface_idx(static_cast<uint8_t>(interface_id))
+        : interface_id(interface_id)
 {}
 
 const MouseInterface& MouseInterfaceInfoEntry::Interface() const
 {
-	assert(mouse_interfaces[interface_idx]);
-	return *mouse_interfaces[interface_idx].get();
+	return MouseInterface::GetInstance(interface_id);
 }
 
 const MousePhysical& MouseInterfaceInfoEntry::MappedPhysical() const
@@ -216,84 +216,42 @@ private:
 
 void MouseInterface::InitAllInstances()
 {
-	if (!mouse_interfaces.empty()) {
-		return; // already initialized
+	for (auto interface_id : AllMouseInterfaceIds) {
+		auto& interface = MouseInterface::GetInstance(interface_id);
+		interface.Init();
+		interface.UpdateConfig();
 	}
+}
 
-	const auto first = static_cast<uint8_t>(MouseInterfaceId::First);
-	const auto last  = static_cast<uint8_t>(MouseInterfaceId::Last);
+MouseInterface& MouseInterface::GetInstance(const MouseInterfaceId interface_id)
+{
+	const auto index = enum_val(interface_id);
+	if (!mouse_interfaces[index]) {
 
-	for (uint8_t i = first; i <= last; i++) {
-		switch (static_cast<MouseInterfaceId>(i)) {
+		switch (interface_id) {
 		case MouseInterfaceId::DOS:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceDos>());
+			mouse_interfaces[index] = new InterfaceDos();
 			break;
 		case MouseInterfaceId::PS2:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfacePS2>());
+			mouse_interfaces[index] = new InterfacePS2();
 			break;
 		case MouseInterfaceId::COM1:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceCOM>(0));
+			mouse_interfaces[index] = new InterfaceCOM(0);
 			break;
 		case MouseInterfaceId::COM2:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceCOM>(1));
+			mouse_interfaces[index] = new InterfaceCOM(1);
 			break;
 		case MouseInterfaceId::COM3:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceCOM>(2));
+			mouse_interfaces[index] = new InterfaceCOM(2);
 			break;
 		case MouseInterfaceId::COM4:
-			mouse_interfaces.emplace_back(
-			        std::make_unique<InterfaceCOM>(3));
+			mouse_interfaces[index] = new InterfaceCOM(3);
 			break;
 		default: assert(false); break;
 		}
 	}
 
-	for (auto& interface : mouse_interfaces) {
-		assert(interface);
-		interface->Init();
-		interface->UpdateConfig();
-	}
-}
-
-MouseInterface* MouseInterface::Get(const MouseInterfaceId interface_id)
-{
-	const auto idx = static_cast<size_t>(interface_id);
-	if (idx < mouse_interfaces.size()) {
-		assert(mouse_interfaces[idx]);
-		return mouse_interfaces[idx].get();
-	}
-
-	assert(interface_id == MouseInterfaceId::None);
-	return nullptr;
-}
-
-MouseInterface* MouseInterface::GetDOS()
-{
-	const auto idx = static_cast<uint8_t>(MouseInterfaceId::DOS);
-	return MouseInterface::Get(static_cast<MouseInterfaceId>(idx));
-}
-
-MouseInterface* MouseInterface::GetPS2()
-{
-	const auto idx = static_cast<uint8_t>(MouseInterfaceId::PS2);
-	return MouseInterface::Get(static_cast<MouseInterfaceId>(idx));
-}
-
-MouseInterface* MouseInterface::GetSerial(const uint8_t port_id)
-{
-	if (port_id < SERIAL_MAX_PORTS) {
-		const auto idx = static_cast<uint8_t>(MouseInterfaceId::COM1) + port_id;
-		return MouseInterface::Get(static_cast<MouseInterfaceId>(idx));
-	}
-
-	LOG_ERR("MOUSE: Ports above COM4 not supported");
-	assert(false);
-	return nullptr;
+	return *mouse_interfaces[index];
 }
 
 MouseInterface::MouseInterface(const MouseInterfaceId interface_id,
@@ -382,6 +340,12 @@ void MouseInterface::NotifyInterfaceRate(const uint16_t new_rate_hz)
 	interface_rate_hz = new_rate_hz;
 	UpdateRate();
 }
+
+void MouseInterface::NotifyMoved(const float, const float, const float, const float) {}
+
+void MouseInterface::NotifyButton(const MouseButtonId, const bool) {}
+
+void MouseInterface::NotifyWheel(const float) {}
 
 void MouseInterface::NotifyBooting() {}
 